c4bdbe3b16e64b950ceb1bf9fb8ac8ae24b69fda
linwenjun <linwenjun@163.com>
2020-02-13 10:58:00 +0800

    add demo for mysterious name

---
+++ src/main/java/smell/$01_mysterious_name/demo/User.java
@@ -0,0 +1,13 @@
+package smell.$01_mysterious_name.demo;
+
+public class User {
+
+    String ad; // 地址
+    String sex; // 性别
+
+    int vehiclesCount; //车数量
+    String[] CarNumber; //车牌号
+
+    String friendList =  "<ul><li>aaa</li><li>bbb</li></ul>"; // 朋友视图列表
+    User[] friendsList; // 朋友列表
+}
