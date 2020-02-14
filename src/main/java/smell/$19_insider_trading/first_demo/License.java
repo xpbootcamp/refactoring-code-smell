fd6c466118c97e173a84336638ce6a981511b60b
linwenjun <linwenjun@163.com>
2020-02-14 20:49:42 +0800

    add demo for insider trading

---
+++ src/main/java/smell/$19_insider_trading/first_demo/License.java
@@ -0,0 +1,23 @@
+package smell.$19_insider_trading.first_demo;
+
+public class License {
+    private Motorist motorist;
+    private int points = 0;
+
+    public void setMotorist(Motorist motorist) {
+        this.motorist = motorist;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void addPoints (int points) {
+        this.points += points;
+    }
+
+    public String getSummary() {
+        return motorist.getTitle() + " " + motorist.getFirstName()
+                + " " + motorist.getFirstName() + ", " + Integer.toString(getPoints());
+    }
+}
