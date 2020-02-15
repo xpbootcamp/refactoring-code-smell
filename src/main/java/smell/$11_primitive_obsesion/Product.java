37537ab4998468f82113f77a83f964523de1a3b7
Liu Lu <yacht2005@gmail.com>
2020-02-15 12:02:56 +0800

    add primitive obsession

---
+++ src/main/java/smell/$11_primitive_obsesion/Product.java
@@ -0,0 +1,14 @@
+package smell.$11_primitive_obsesion;
+import lombok.Data;
+
+@Data
+public class Product {
+    private String category;
+    private String itemType;
+    private String brand;
+    private String name;
+    private double purchasePrice;
+    private String purchaseCurrency;
+    private double sellPrice;
+    private String SellCurrency;
+}
