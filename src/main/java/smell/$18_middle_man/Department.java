82702fa0a22917484ba1b6a9c107976c3f04fda6
Yuan Shenjian <sjyuan@thoughtworks.com>
2020-02-15 11:52:36 +0800

    Add demo for [Middle Man]
    Signed-off-by: Yuan Shenjian <sjyuan@thoughtworks.com>

---
+++ src/main/java/smell/$18_middle_man/Department.java
@@ -0,0 +1,24 @@
+package smell.$18_middle_man;
+
+public class Department {
+    private String name;
+    private long membersCount;
+    private String type;
+    private Person manager;
+
+    public Department(Person manager) {
+        this.manager = manager;
+    }
+
+    public Person getManager() {
+        return manager;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
