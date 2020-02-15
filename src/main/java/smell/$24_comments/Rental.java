63803a7ca41dc5e720299011a306fca8afc040a7
Yuan Shenjian <sjyuan@thoughtworks.com>
2020-02-15 10:39:20 +0800

    Add demo for [Comments]
    Signed-off-by: Yuan Shenjian <sjyuan@thoughtworks.com>

---
+++ src/main/java/smell/$24_comments/Rental.java
@@ -0,0 +1,21 @@
+package smell.$24_comments;
+
+public class Rental {
+
+    private Movie movie;
+    private int daysRented;
+
+    Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
+    }
+
+    Movie getMovie() {
+        return movie;
+    }
+
+    int getDaysRented() {
+        return daysRented;
+    }
+
+}
